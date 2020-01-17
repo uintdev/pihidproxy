d0226e86171cd2e53faa46f0728c73cd225724c0
Andre Santos <uintdev@users.noreply.github.com>
2020-01-17 02:28:17 +0000

    Improve device detection and support spaces
    Add support for Bluetooth device names with spaces
    Using `rfcomm` as `bluetoothctl scan on` takes too long (unknown how long it would actually take, but it was unacceptable for this sort of project)

--- pair.sh
+++ pair.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-devicename="NexDock"
+devicename="Bluetooth Keyboard"
 
 echo "Finding BT device *$devicename* ..."
 scan=$(sudo hcitool scan)
-found=$(grep $devicename <<< $scan)
+found=$(grep "$devicename" <<< $scan)
 if [ $? -eq 0 ]
 then
         mac=$(cut -f 2 <<< $found)
         echo $mac
-        echo "
-agent on
-scan on
-pair $mac
-connect $mac" | sudo bluetoothctl
-
-
+        sudo bluetoothctl agent on
+        sudo rfcomm bind 0 $mac
+        sudo rfcomm connect 0 $mac
+        sudo bluetoothctl pair $mac
+        sudo bluetoothctl trust $mac
+        sudo bluetoothctl connect $mac
 else
         echo not found
 fi
